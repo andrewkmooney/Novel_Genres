62239c3c234f3c5d34781ab583d3e7bd63f42243
Andrew Mooney <amooney@Andrews-MacBook-Pro.local>
2021-07-01 09:34:13 -0500

    Saved models for age and genre

--- firt_app.py
+++ firt_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from PIL import Image
+
+image = Image.open('—Pngtree—blue technology city building cool_968565.png')
+
+st.image(image)
